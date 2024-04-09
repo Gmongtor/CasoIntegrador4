f1437f6a25cd0cba559eb019a2d77cc35816d3c9
Gmongtor <gmongtor@myuax.com>
2024-04-09 23:24:22 +0200

    Ultimos retoques

---
+++ Caso4/src/test/java/Main/MenuTest.java
@@ -0,0 +1,18 @@
+package Main;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MenuTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void frameInit() {
+    }
+}
